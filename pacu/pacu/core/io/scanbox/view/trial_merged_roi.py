231a501262f60ed0da4e966fa6771d1836f2fb9f
jzeitoun <jzeitoun@uci.edu>
2018-02-13 13:00:30 -0800

    fixed params.cell_id value in backend and updated stitcher to handle cases with no params.cell_id

--- pacu/pacu/core/io/scanbox/view/trial_merged_roi.py
+++ pacu/pacu/core/io/scanbox/view/trial_merged_roi.py
@@ -39,24 +39,27 @@ class TrialMergedROIView(object):
     r.refresh() # recompute every datatag
     """
     exc_no_roi = 'One of workspaces does not have an ROI with id {.roi_id}'
-    def __init__(self, roi_id, version=2, *workspaces):
+    def __init__(self, roi_id, *workspaces):
         self.roi_id = roi_id
         self.workspaces = workspaces
         self.workspace = self.workspaces[0]
         self.condition = self.workspace.condition
-        self.version = version
+        #self.version = version
         self.rois = self.collect_rois()
     def collect_rois(self):
         try:
-            if self.version == 1:
-                return [
-                    ws.rois.filter_by(params={'cell_id': str(self.roi_id)})[0]
-                for ws in self.workspaces]
-            elif self.version == 2:
+            return [
+                ws.rois.filter_by(params={'cell_id': str(self.roi_id)})[0]
+                    for ws in self.workspaces
+                    ]
+        except:
+            try:
                 return [
                     ws.rois.filter_by(id=self.roi_id)[0]
-                for ws in self.workspaces]
-        except: raise Exception(self.exc_no_roi.format(self))
+                        for ws in self.workspaces
+                        ]
+            except:
+                raise Exception(self.exc_no_roi.format(self))
     @memoized_property
     def dff0s(self):
         return flist([datatag for roi in self.rois for datatag in roi.dttrialdff0s])
